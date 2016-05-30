46377acdde610c5d1704e352321ebb2ebe3f3f38
Erik Linder Norén <erino397@li4-2.ida.liu.se>
2016-05-30 11:18:32 +0200

    New script for setting up the project on IDA computers. Readme file with instructions

---
+++ linuxpintos/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+#Setup
+#!!! Make sure linuxpintos folder is placed in home directory !!!
+
+cd src/utils
+make clean
+ln -s $(which qemu-system-i386) qemu
+make
+chmod +x pintos
+chmod +x pintos-mkdisk
+chmod +x backtrace
+chmod +x pintos-gdb
+export PATH="${HOME}/linuxpintos/src/utils/:${PATH}/"
+cd ../threads
+make clean
+make
+cd build
+pintos --qemu -- run alarm-multiple
--- linuxpintos/src/threads/thread.c
+++ linuxpintos/src/threads/thread.c
@@ -185,7 +185,7 @@ thread_create (const char *name, int priority,
 
   
   /* Initialize thread. */
-  init_thread (t, name, prio
+  init_thread (t, name, priority);
   tid = t->tid = allocate_tid ();
   
   /* Stack frame for kernel_thread(). */
